5e9ca3f6978d50a3015b96247a74ec3a4dd5f724
yanuvelir <yanuvelir@gmail.com>
2023-02-13 17:00:22 +0200

    Initial commit

---
+++ src/testJava.java
@@ -0,0 +1,19 @@
+public class testJava {
+
+    public static void main(String[] args) {
+        System.out.println("Automation");
+        String wpt, google, microsoft;
+        wpt = "best";
+        google = "not really";
+        microsoft = "worst";
+        System.out.println(wpt +" "+ google + " "+ microsoft);
+
+        int age, legs, tail;
+        age= 12;
+        legs =4;
+        tail=1;
+        int sum = age+legs+tail;
+        System.out.println(sum);
+
+    }
+}
